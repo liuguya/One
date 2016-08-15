7f108258685ab92aad518b49c91cbc8d44e0a457
阳阳 <zhangsan@qq.com>
2016-08-15 18:46:12 +0800

    创建首页标签视图控制器

---
+++ One/One/classes/common/LFConst.swift
@@ -0,0 +1,16 @@
+//
+//  LFConst.swift
+//  LimitFree1606
+//
+//  Created by gaokunpeng on 16/7/26.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import UIKit
+
+//获取屏幕的宽度
+public let kScreenW = UIScreen.mainScreen().bounds.size.width
+//获取屏幕的高度
+public let kScreenH = UIScreen.mainScreen().bounds.size.height
+
+
---
+++ One/One/classes/common/MainTabBarController.swift
@@ -0,0 +1,47 @@
+//
+//  MainTabBarController.swift
+//  One
+//
+//  Created by 阳阳 on 16/8/15.
+//  Copyright © 2016年 liuguyang. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    var tabView:UIView?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+       createViewControllers()
+ 
+    }
+    
+    func createViewControllers(){
+        let ctrlNames = ["HomeViewController","ReadViewController","MusicViewController","MovieViewController"]
+        
+        let images = ["app_home@1","app_read@1","app_music@1","app_movie@1"]
+        let selectImages = ["app_home@1","app_read@1","app_music@1","app_movie@1"]
+        let names = ["首页","阅读","音乐","电影"]
+        
+        var vCtrlArray = Array<UINavigationController>()
+        for i in 0..<ctrlNames.count{
+            let ctrlName = "One." + ctrlNames[i]
+            let cls = NSClassFromString(ctrlName) as! UIViewController.Type
+            
+            let ctrl = cls.init()
+            
+            ctrl.tabBarItem.title = names[i]
+            ctrl.tabBarItem.image = UIImage(named: images[i])
+            ctrl.tabBarItem.selectedImage = UIImage(named: selectImages[i])
+            //导航
+            let navCtrl = UINavigationController(rootViewController: ctrl)
+            vCtrlArray.append(navCtrl)
+            
+        }
+        self.viewControllers = vCtrlArray
+    }
+ 
+
+}
